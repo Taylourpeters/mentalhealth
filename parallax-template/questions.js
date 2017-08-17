f76c53185e7c937f9e421d739c2826cd214c892f
ForeverClemy <clemreyn3@charter.newvisions.org>
2017-08-17 14:05:53 -0400

    yum

--- parallax-template/questions.js
+++ parallax-template/questions.js
@@ -1,12 +1,41 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    var questions = ["Did you fight with a friend?", "Do you have another friend to confide in?", "Do you wish you were different physically?", "Did you harm yourself physically?", "Are you thinking of harming yourself physically in any way?", "would you consider seeing a therapist?", "Do you have panic attacks?", "Did you go to work today?", "Do you work alot?", "Do you like what you do?", "Even so, is it sometimes overwhelming?", "Do you feel that you are sometimes too sensitive and get hurt too easily?", "First question for taylour6-7?", "First question for taylour8-10?", "Fill in answer for now?"];
+    var questions = ["Did you fight with a friend?", "Do you have another friend to confide in?", "Do you wish you were different physically?", "Have you intentionally harmed yourself physically in any way?", "Do you intend to inflict self harm in any way?", "Would you consider seeing a therapist?", "Do you have panic attacks?", "Did you go to work today?", "Do you work alot?", "Do you like what you do?", "Even so, is it sometimes overwhelming?", "Do you feel that you are sometimes too sensitive and get hurt too easily?", "Did you have a good day at school or work?", "Was it because of someone else?", "Did you feel appreciated today?", "Did someone special make you feel appreciated?", "Did someone neglect you today?", "Do you like this person?", "Do you want a relationship to happen with this person?", "Did you hang out with friends or family", "Do you usually get neglected?", "Is it the same person that neglects you?","We are guessing since you rated your day so high, it was a good day... Were we right?", "Did you do activities you like today?", "Did you go to work?", "Did you have fun?", "Was today good because you weren't overwhelmed?", "Do you have a hobby you can do?", "Does that hobby calm you?", "Did someone make an assumption about you that you didn't like?", "Did this help", "Did this help?","Filler answer", "yes thanks"];
 
     var next = {
-    			0:[1, 2],
-    			1:[14, 5],
-    			2:[3, ],
+    			0:[1,2],
+    			1:[32,6],
+    			2:[6,11],
+                3:[32,4],
+                4:[32,27],
+                5:[32,3],
+                6:[5,5],
+                7:[8,29],
+                8:[9,29],
+                9:[19,32],
+                10:[32,29],
+                11:[32,7],
+                // 12:[,],
+                // 13:[,],
+                // 14:[,],
+                // 15:[,],
+                // 16:[,],
+                // 17:[,],
+                // 18:[,],
+                // 19:[,],
+                // 20:[,],
+                // 21:[,],
+                // 22:[,],
+                // 23:[,],
+                // 24:[,],
+                // 25:[,],
+                // 26:[,],
+                27:[28,32],
+                28:[32,32],
+                29:[32,1],
+                30:[33,3],
+                31:[33,5],
     		};
 
     var currentQuestionIndex = 0;
